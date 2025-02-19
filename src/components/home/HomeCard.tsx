98deb6c27d6a1c82211cf554f9eb87d13c3af9a0
MhrrmliEltac <meherremlieltac14@gmail.com>
2025-02-19 14:26:38 +0400

    HomeCard Update

--- src/components/home/HomeCard.tsx
+++ src/components/home/HomeCard.tsx
@@ -1,5 +1,9 @@
 import { useNavigate } from "react-router";
-import ItemCard from "./ItemCard";
+import { motion } from "framer-motion";
+import flower from "../../assets/flower.svg";
+import cake from "../../assets/cake.svg";
+import chocolate from "../../assets/chocolate.svg";
+import plant from "../../assets/plant.svg";
 
 type cardItemType = {
   id: number;
@@ -12,25 +16,25 @@ const cardItem: cardItemType[] = [
   {
     id: 1,
     title: "Flowers & Bouquets",
-    image: "/src/assets/flower.svg",
+    image: flower,
     url: "/flowers",
   },
   {
     id: 2,
     title: "Cakes & Pastries",
-    image: "/src/assets/cake.svg",
+    image: cake,
     url: "/cakes",
   },
   {
     id: 3,
     title: "Premium Chocolates",
-    image: "/src/assets/chocolate.svg",
+    image: chocolate,
     url: "/chocolates",
   },
   {
     id: 4,
     title: "Potted Plants",
-    image: "/src/assets/plant.svg",
+    image: plant,
     url: "/plants",
   },
 ];
@@ -41,16 +45,76 @@ const HomeCard = () => {
     navigate(path);
   };
 
+  const containerVariant = {
+    hidden: { opacity: 0 },
+    visible: {
+      opacity: 1,
+      transition: {
+        staggerChildren: 0.2,
+      },
+    },
+  };
+
+  const imgVariant = {
+    initial: { scale: 1.1, transition: { duration: 0.6 } },
+    hover: { scale: 1.2, transition: { duration: 0.6 } },
+  };
+
+  const textVariant = {
+    initial: { opacity: 1, transition: { duration: 0.3 } },
+    hover: { opacity: 0, transition: { duration: 0.3 } },
+  };
+
+  const parentVariant = {
+    hidden: { opacity: 0 },
+    visible: {
+      opacity: 0,
+      transition: {
+        duration: 4,
+      },
+    },
+  };
+
+  const childVariant = {
+    hidden: { opacity: 0 },
+    visible: { opacity: 1, transition: { duration: 0.5 } },
+  };
+
   return (
-    <div className="flex justify-between flex-wrap w-full">
+    <motion.div
+      className="grid lg:grid-cols-4 md:grid-cols-2 grid-cols-1 w-full gap-5 p-4"
+      variants={parentVariant}
+    >
       {cardItem.map((item: cardItemType) => (
-        <ItemCard
-          key={item.id}
-          cardItem={item}
+        <motion.div
+          className="relative duration-300 shadow-lg hover:shadow-2xl rounded-[30px] lg:aspect-[315/380] aspect-[950/380] overflow-hidden cursor-pointer"
           onClick={() => navigateTo(item.url)}
-        />
+          initial="initial"
+          whileHover="hover"
+          key={item.id}
+        >
+          <div className="absolute inset-0">
+            <motion.img
+              variants={imgVariant}
+              src={item.image}
+              alt={item.title}
+              className="w-full h-full object-cover"
+            />
+            <motion.div
+              className="absolute inset-0 bg-black/50 backdrop-blur-md"
+              variants={textVariant}
+            ></motion.div>
+          </div>
+
+          <motion.div
+            className="relative z-10 flex items-center justify-center h-full text-white text-center font-bold text-xl lg:text-3xl px-6"
+            variants={textVariant}
+          >
+            <p>{item.title}</p>
+          </motion.div>
+        </motion.div>
       ))}
-    </div>
+    </motion.div>
   );
 };
 

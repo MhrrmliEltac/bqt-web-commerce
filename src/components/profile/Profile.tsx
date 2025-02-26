16ba30085dc8abf620d5ca911f82bbfe1d13b709
MhrrmliEltac <meherremlieltac14@gmail.com>
2025-02-26 14:13:48 +0400

    Profile

--- src/components/profile/Profile.tsx
+++ src/components/profile/Profile.tsx
@@ -1,9 +1,35 @@
 import Heading from "../general/Heading";
 import profile from "../../assets/profile.svg";
 import edit from "../../assets/edit-icon.svg";
-import { Box, FormControl, FormLabel } from "@mui/material";
+import { Box, duration, FormControl, FormLabel } from "@mui/material";
+import { useState } from "react";
+import { motion } from "framer-motion";
+
+interface User {
+  name: string;
+  email: string;
+  phone: string;
+}
 
 const Profile = () => {
+  const [activeTab, setActiveTab] = useState<string>("Personal info");
+  const [disable, setDisabled] = useState<boolean>(false);
+  const [inputValue, setInputValue] = useState<User[]>([
+    {
+      name: "Edward Norton",
+      email: "emailtest@mail.com",
+      phone: "+994 50 123 45 67",
+    },
+  ]);
+
+  const handleChangeActive = (text: string) => {
+    setActiveTab(text);
+  };
+
+  const handleInputDisable = () => {
+    setDisabled(!disable);
+  };
+
   return (
     <section>
       <div>
@@ -11,18 +37,21 @@ const Profile = () => {
       </div>
       <div className="mt-10 flex flex-col gap-10  mb-10">
         <ul className="flex justify-between items-center w-full gap-5 md:w-[75%] text-lg">
-          <li className="border-b border-[#2C4B42] font-kodchasan cursor-pointer">
-            Personal info
-          </li>
-          <li className="border-b border-[#2C4B42] font-kodchasan cursor-pointer">
-            My addresses
-          </li>
-          <li className="border-b border-[#2C4B42] font-kodchasan cursor-pointer">
-            My cards
-          </li>
-          <li className="border-b border-[#2C4B42] font-kodchasan cursor-pointer">
-            Change password
-          </li>
+          {["Personal info", "My addresses", "My cards", "Change password"].map(
+            (item, index) => (
+              <motion.li
+                initial={{ transition: { duration: 0.6 } }}
+                animate={{ transition: { duration: 0.6 } }}
+                onClick={() => handleChangeActive(item)}
+                key={index}
+                className={`${
+                  activeTab === item && "border-b-2"
+                } border-[#2C4B42] font-kodchasan cursor-pointer text-[22px] text-[#2C4B42] font-[500]`}
+              >
+                {item}
+              </motion.li>
+            )
+          )}
         </ul>
         <div className="flex flex-col gap-10 mt-5 ">
           <div className="rounded-full relative">
@@ -60,11 +89,13 @@ const Profile = () => {
                 type="name"
                 name="name"
                 id="name"
-                disabled
-                value="Edward Norton"
-                className="font-[400] font-kodchasan text-[#2c4b42] text-lg bg-transparent outline-none"
+                disabled={disable}
+                value={inputValue[0].name}
+                className="font-[400] font-kodchasan text-[#2C4B42] text-lg bg-transparent outline-none"
               />
-              <p className="cursor-pointer">Edit</p>
+              <p className="cursor-pointer text-[#2C4B42] text-base font-medium">
+                Edit
+              </p>
             </Box>
           </FormControl>
           <FormControl>
@@ -90,11 +121,13 @@ const Profile = () => {
                 type="tel"
                 name="phone"
                 id="phone"
-                disabled
-                value="+994 50 123 45 67"
+                disabled={disable}
+                value={inputValue[0].phone}
                 className="font-[400] font-kodchasan text-[#2c4b42] text-lg bg-transparent outline-none"
               />
-              <p className="cursor-pointer">Edit</p>
+              <p className="cursor-pointer text-[#2c4b42] text-base font-medium">
+                Edit
+              </p>
             </Box>
           </FormControl>
           <FormControl>
@@ -120,11 +153,16 @@ const Profile = () => {
                 type="email"
                 name="email"
                 id="email"
-                value="emailtest@mail.com"
-                disabled
+                value={inputValue[0].email}
+                disabled={disable}
                 className="font-[400] font-kodchasan text-[#2c4b42] text-lg bg-transparent outline-none"
               />
-              <p className="cursor-pointer">Edit</p>
+              <p
+                onClick={handleInputDisable}
+                className="cursor-pointer text-[#2c4b42] text-base font-medium"
+              >
+                Edit
+              </p>
             </Box>
           </FormControl>
         </div>

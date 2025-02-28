8068d9f0da268e761935e1ec0fabef2867844a04
MhrrmliEltac <meherremlieltac14@gmail.com>
2025-02-28 16:25:01 +0400

    Added Loading Bar

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,11 @@
 import "./App.css";
-import { ToastContainer } from "react-toastify";
+
 import "react-toastify/dist/ReactToastify.css";
 import AuthLayout from "./components/auth/AuthLayout";
 import Login from "./components/auth/Login";
 import PasswordReset from "./components/auth/PasswordReset";
 import Register from "./components/auth/Register";
 import Verification from "./components/auth/Verification";
-import {
-  BrowserRouter as Router,
-  Route,
-  Routes,
-  Navigate,
-} from "react-router-dom";
 import Home from "./components/pages/Home";
 import Flowers from "./components/MainFlow/flowers/Flowers";
 import FlowerDetail from "./components/MainFlow/flowerDetail/FlowerDetail";
@@ -22,13 +16,32 @@ import NotFoundPage from "./components/pages/NotFoundPage";
 import Layout from "./components/layout/Layout";
 import OrderDetail from "./components/order/OrderDetail";
 import PaymentDetail from "./components/payment/PaymentDetail";
-import { Toaster } from "sonner";
 import ProfileLayout from "./components/profile/ProfileLayout";
+import {
+  BrowserRouter as Router,
+  Route,
+  Routes,
+  Navigate,
+} from "react-router-dom";
+import { ToastContainer } from "react-toastify";
+import { Toaster } from "sonner";
+import TopLoadingBar from "react-top-loading-bar";
+import { useState } from "react";
+import ProgressBar from "./components/progressbar/ProgressBar";
 
 function App() {
+  const [progress, setProgress] = useState(0);
+
   return (
     <Router>
       <Toaster />
+      <ProgressBar />
+      <TopLoadingBar
+        color="#2C4B42" // İstədiyin rəngi seç
+        progress={progress}
+        onLoaderFinished={() => setProgress(0)}
+        height={4}
+      />
       <Routes>
         <Route element={<Layout />}>
           <Route path="/" element={<Home />} />
